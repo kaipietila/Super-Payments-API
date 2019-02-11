08134e881c23126b6245000bd1ece281ac9f6bbd
kaipietila <kai.m.pietila@gmail.com>
2019-02-11 21:02:51 +0200

    Added view outlines

--- fintech/views.py
+++ fintech/views.py
@@ -1,5 +1,24 @@
-from .models import Account, Transaction
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Account, Transaction
+from .serializers import AccountSerializer, TransactionSerializer
 
+class TransactionList(APIView):
+    """
+    Get a List of all Transactions from an account
+    """
+    def get(self, request, uuid):
+        account = Account.object.get(uuid)
+        transactions = account.transactions.all()
+        serializer = TransactionSerializer(transactions, many=True)
+        return Response(serializer.data)
 
+class AccountDetail(APIView):
+    """
+    Get details of one account
+    """
+    pass
 
+class AccountBalanceDetails(APIView):
+    pass
